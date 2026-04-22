19f0b5e04f461756371019b52931255a10d78a5f
muhos <muh.osama25@gmail.com>
2026-04-22 14:57:03 +0200

    cleaned up script

--- run_benchmarks/synthesis/syn_ccx.py
+++ run_benchmarks/synthesis/syn_ccx.py
@@ -16,20 +16,14 @@ qasmfile = args.qasmfile
 Synthesis
 '''
 
-# # Parse the circuits
-# circuit = qk.encoding.QASMparser(qasmfile)
-# # Get (circuit)^dagger
-# circuit.dagger()
-# # Get CNF for the circuit in Pauli basis (can change to True for the computational basis)
-# cnf = qk.encoding.QASM2CNF(circuit, computational_basis = False)
-# result, weight, solution, layers = qk.Synthesis(cnf, cnf_dir, onehot_xz = True)
-result, weight, solution, layers = qk.functionalities.syn(qasmfile, basis="pauli", cyc_lin_encoding=True, fid=1, files_root=cnf_dir, gate_set={'h', 'cx', 'csqrtx'})
-print("Solution: ")
-print(solution)
-# The result will be "FOUND" if a solution was found, 
+# The 'result' will be "FOUND" if a solution was found, 
 # "CRASH" if there was a problem such as an invalid cnf or not enough mem, 
 # "ERROR#" if the tool finished with an error, and "TIMEOUT" if the tool ran out of time.
 # In the case of "TIMEOUT", the best solution found will be returned.
-# weight will give the achieved fidelity (should be 1 if "FOUND", less if "TIMEOUT") of the (best) found circuit.
-# solution will be a string in a qasm file format describing the (best) circuit found, achieving the mentioned weight.
+
+# 'weight' will give the achieved fidelity (should be 1 if "FOUND", less if "TIMEOUT") of the (best) found circuit.
+
+# 'solution' will be a string in a qasm file format describing the (best) circuit found, achieving the mentioned weight.
+
 # 'layers' is an integer stating the achieved minimal depth
+result, weight, solution, layers = qk.functionalities.syn(qasmfile, basis="pauli", cyc_lin_encoding=True, fid=1, files_root=cnf_dir, gate_set={'h', 'cx', 'csqrtx'})
