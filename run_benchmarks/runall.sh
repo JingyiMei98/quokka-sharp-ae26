00ad2c8903b2aeea202644ebb51df602de04c80b
JingyiMei98 <143427059+JingyiMei98@users.noreply.github.com>
2026-04-26 20:06:38 +0800

    Update runall.sh

--- run_benchmarks/runall.sh
+++ run_benchmarks/runall.sh
@@ -40,7 +40,8 @@ echo "=== Simulation results comparison ==="
 # run_py compare_simulation.py -a Feynman -b benchlist-feymann.txt
 run_py compare_simulation.py -a origin -b benchlist-sim.txt -t quokka-gpmc quokka-ganak quasimodo ddsim
 run_py compare_simulation.py -a origin -b benchlist-sim-add.txt
-run_py compare_simulation.py -a ModifiedRevlib -b benchlist-mrevlib.txt
+run_py compare_simulation.py -a Feynman -b benchlist-feymann.txt
+run_py compare_simulation.py -a ModifiedRevLib -b benchlist-mrevlib.txt
 run_py compare_simulation.py -a revlib -b benchlist-revlib.txt
 
 echo "=== Equivalence checking ==="
